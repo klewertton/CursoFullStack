6f419e4c64c62a30bfe919629723e800022d9add
klewertton <klewertton.tenorio@gmail.com>
2025-02-21 20:35:32 -0300

    Revert "commit 21-02-25"
    This reverts commit b871cfd8f00be382c36279ad32ea62c2c9bcfb92.

--- api-desafio/src/main/java/inicial_spring_desafio/api/repository/GatoRepository.java
+++
@@ -1,9 +0,0 @@
-package inicial_spring_desafio.api.repository;
-
-import inicial_spring_desafio.api.model.Gato;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GatoRepository extends JpaRepository<Gato, Long> {
-}
--- api-desafio/src/main/java/inicial_spring_desafio/api/repository/RestauranteRepository.java
+++
@@ -1,9 +0,0 @@
-package inicial_spring_desafio.api.repository;
-
-import inicial_spring_desafio.api.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
-}
